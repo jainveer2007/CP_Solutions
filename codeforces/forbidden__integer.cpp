bfae09711a223fd76150f766875be037ec862124
jainveer2007 <jainveer2007@gmail.com>
2026-06-03 17:49:39 +0530

    question added

---
+++ codeforces/forbidden__integer.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define L long long 
+#define Ma INT_MAX
+#define Mi INT_MIN
+
+int main() {
+
+    int t;
+    cin>>t;
+
+    while(t--) {
+
+        L n,k,x;
+        cin>>n>>k>>x;
+
+        
+    }
+}
