2a566f9f305909677dc16ed3c565a02ace07b90e
jainveer2007 <jainveer2007@gmail.com>
2026-06-04 19:08:23 +0530

    added

--- codeforces/forbidden__integer.cpp
+++ codeforces/forbidden__integer.cpp
@@ -15,6 +15,33 @@ int main() {
         L n,k,x;
         cin>>n>>k>>x;
 
-        
+        if(x != 1) {
+            cout<<"YES"<<endl;
+            cout<<n<<endl;
+            for(int i=0;i<n;i++) cout<<1<<" ";
+            cout<<endl;
+        }
+        else {
+            if(n == 2 && k >= 2) {
+                cout<<"YES"<<endl;
+                cout<<1<<endl;
+                cout<<2<<endl;
+            }
+            else if(n % 2 == 0 && k >= 2) {
+                cout<<"YES"<<endl;
+                cout<<2<<endl;
+                cout<<2<<" "<<n-2<<endl;
+            }
+            else if(n % 2 == 1 && k >= 3) {
+                cout<<"YES"<<endl;
+                cout<<3<<endl;
+                cout<<2<<" "<<2<<" "<<n-4<<endl;
+            }
+            else {
+                cout<<"NO"<<endl;
+            }
+        }
     }
+
+    return 0;
 }
