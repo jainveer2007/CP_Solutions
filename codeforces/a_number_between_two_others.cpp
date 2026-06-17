12a42f09e3781c612dd58ceac86af0e8dde43dd7
jainveer2007 <jainveer2007@gmail.com>
2026-06-17 21:27:05 +0530

    question solved

---
+++ codeforces/a_number_between_two_others.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define L long long 
+
+int main() {
+
+    L t;
+    cin>>t;
+
+    while(t--) {
+
+        L x,y;
+        cin>>x>>y;
+
+        bool found = false;
+
+        if (y == 2 * x) {
+            cout<<"NO"<<endl;
+        }
+        else{
+            cout<<"YES"<<endl;
+        }
+    }
+    return 0;
+}
