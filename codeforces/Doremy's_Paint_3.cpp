77364f2188aa0719c1de472ffe1947007c8bf813
jainveer2007 <jainveer2007@gmail.com>
2026-06-17 08:08:17 +0530

    question solved

--- codeforces/Doremy's_Paint_3.cpp
+++ codeforces/Doremy's_Paint_3.cpp
@@ -11,23 +11,41 @@ int main() {
         int n;
         cin>>n;
 
-        int arr[n];
+        vector<int> arr(n);
 
         for(int i=0; i<n; i++) {
             cin>>arr[i];
         }
 
-        if (n==2) {
-            cout<<"YES"<<endl;
-        }
+        sort(arr.begin(), arr.end());
+
+        int first = arr[0];
+        int last = arr[n-1];
+        bool good = true;
 
         for(int i=0; i<n; i++) {
 
-            if(arr[i]+arr[i+1] == arr[i+2]+arr[i+3]) {
-                cout<<"YES"<<endl;
+            if(arr[i] != first && arr[i] != last) {
+                good = false;
+                break;
             }
+        }
+
+        if(!good) {
+            cout<<"No"<<endl;
+        }
+        else if(first == last) {
+            cout<<"Yes"<<endl;
+        }
+        else {
+            int cnt1 = count(arr.begin(), arr.end(), first);
+            int cnt2 = count(arr.begin(), arr.end(), last);
+
+            if(abs(cnt1 - cnt2) <= 1) {
+                cout<<"Yes"<<endl;
+            } 
             else {
-                cout<<"NO"<<endl;
+                cout<<"No"<<endl;
             }
         }
     }
