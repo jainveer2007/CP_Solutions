dafbf00c84ffd5b5104b10b6eff5dc90e29d6a3f
jainveer2007 <jainveer2007@gmail.com>
2026-06-18 21:26:53 +0530

    question soved

--- codeforces/Zhily_And_Array_Operating.cpp
+++ codeforces/Zhily_And_Array_Operating.cpp
@@ -1,23 +1,37 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define L long long 
+#define L long long
 
 int main() {
 
-    L t;
+    int t;
     cin>>t;
 
     while(t--) {
 
-        L n;
+        int n;
         cin>>n;
 
-        vector<L> v(n)
+        vector<L> a(n), dp(n);
 
         for(int i=0; i<n; i++) {
-            cin>>v[i]
+            cin>>a[i];
         }
+
+        dp[n - 1] = a[n - 1];
+
+        for(int i=n-2; i>=0; i--) {
+            dp[i] = max(a[i], a[i] + dp[i + 1]);
+        }
+
+        int ans = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(dp[i] > 0) ans++;
+        }
+
+        cout<<ans<<endl;
     }
 
-    return 0;   
+    return 0;
 }
