c0cb02de1dde434c162ab07aa84cfc6d4217a323
jainveer2007 <jainveer2007@gmail.com>
2026-06-26 08:17:30 +0530

    question solved

---
+++ codeforces/Equal_Subsequences.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define L long long 
+
+int main() {
+
+    L t;
+    cin>>t;
+
+    while(t--) {
+
+        L n,k;
+        cin>>n>>k;
+
+        for(int i=0; i<k; i++) {
+            cout<<"1";
+        }
+        
+        for(int i=0; i<n-k; i++) {
+            cout<<"0";
+        }
+
+        cout<<endl;
+    }
+
+    return 0;
+}
