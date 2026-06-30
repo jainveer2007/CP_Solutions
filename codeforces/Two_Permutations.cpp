de5ab7c2420471e1c38d93400d6422e93208c33c
jainveer2007 <jainveer2007@gmail.com>
2026-06-30 17:54:44 +0530

    question solved

--- codeforces/Two_Permutations.cpp
+++ codeforces/Two_Permutations.cpp
@@ -1,13 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define L long long
 
 int main() {
 
-    int t;
+    L t;
     cin>>t;
 
     while(t--) {
 
-        
+        L n,a,b;
+        cin>>n>>a>>b;
+
+        if(a+b+2<=n || (a==b && a==n)) cout<<"Yes"<<endl;
+        else cout<<"No"<<endl;
     }
+
+    return 0;
 }
