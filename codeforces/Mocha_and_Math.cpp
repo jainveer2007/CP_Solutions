d86b5301daaa006edb095d2b7cb2c5d1fa99d9d0
jainveer2007 <jainveer2007@gmail.com>
2026-07-07 11:38:13 +0530

    question to be solved later

---
+++ codeforces/Mocha_and_Math.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define L long long 
+
+int main() {
+
+    L t;
+    cin>>t;
+
+    while(t--) {    
+        
+        L n;
+        cin>>n;
+
+        vector<L> v(n);
+
+        for(int i=0; i<n; i++) {
+            cin>>v[i];
+        }
+
+        
+    }
+}
