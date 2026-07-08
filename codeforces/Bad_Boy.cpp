5b59a7aa59b8cb135ecf7b1b686ac283d210c687
jainveer2007 <jainveer2007@gmail.com>
2026-07-08 10:51:17 +0530

    question solved

---
+++ codeforces/Bad_Boy.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define L long long 
+
+int main() {
+
+    L t;
+    cin>>t;
+
+    while(t--) {    
+        
+        L n,m,i,j;
+        cin>>n>>m>>i>>j;
+
+        cout << 1 << " " << 1 << " " << n << " " << m << endl;
+    }
+
+    return 0;
+}
