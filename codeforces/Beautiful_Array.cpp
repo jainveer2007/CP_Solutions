56dd334ab13d0d552849fa21027667f8348cbc18
jainveer2007 <jainveer2007@gmail.com>
2026-07-31 15:41:20 +0530

    question solved

--- codeforces/Beautiful_Array.cpp
+++ codeforces/Beautiful_Array.cpp
@@ -9,9 +9,35 @@ int main() {
 
     while(t--) {    
         
-        L n,k,b,s;
+        L n, k, b, s;
         cin>>n>>k>>b>>s;
 
-        
+        L minimum_s = (k * b);
+        L maximum_s = (k * b) + (k - 1) * n;
+
+        if(s < minimum_s || s > maximum_s) {
+
+            cout<<-1<<endl;
+
+        }
+        else {
+
+            vector<L> ans(n, 0);
+            ans[0] = minimum_s;
+            s -= minimum_s;
+
+            for(L i = 0; i < n; i++) {
+
+                L add = min(k - 1, s);
+                ans[i] += add;
+                s -= add;
+            }
+
+            for(L i = 0; i < n; i++)
+                cout<<ans[i]<<" ";
+                cout<<endl;
+        }
     }
+
+    return 0;
 }
