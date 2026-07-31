c4b780bee177d85849f01b7c452f38d9682621e3
jainveer2007 <jainveer2007@gmail.com>
2026-07-31 15:03:58 +0530

    changes added

---
+++ codeforces/Beautiful_Array.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define L long long 
+
+int main() {
+
+    L t;
+    cin>>t;
+
+    while(t--) {    
+        
+        L n,k,b,s;
+        cin>>n>>k>>b>>s;
+
+        
+    }
+}
