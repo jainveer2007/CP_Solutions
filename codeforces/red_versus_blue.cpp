761f8a29f45127f9c9e0e717cb44d332f9884f81
jainveer2007 <jainveer2007@gmail.com>
2026-08-05 18:16:59 +0530

    changes added

---
+++ codeforces/red_versus_blue.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define L long long 
+
+int main() {
+
+    L t;
+    cin>>t;
+
+    while(t--) {    
+        
+        L n,r,b;
+        cin>>n>>r>>b;
+
+        
+    }
+}
