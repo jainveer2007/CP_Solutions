a126180a1dbc15ee5849706bfc9fe99f851ad34a
jainveer2007 <jainveer2007@gmail.com>
2026-08-11 22:52:57 +0530

    question solved

--- codeforces/divan_and_new_project.cpp
+++ codeforces/divan_and_new_project.cpp
@@ -8,7 +8,7 @@ int main() {
     cin>>t;
 
     while(t--) {    
-        
+    
         L n;
         cin>>n;
 
@@ -18,9 +18,42 @@ int main() {
             cin>>v[i];
         }
 
-        sort(v.begin(), v.end(), greater<L>());
+        vector<pair<L,L>> building_map;
 
-        
+        for(int i=0; i<n; i++) {
+            building_map.push_back({v[i], i});
+        }
+
+        sort(building_map.rbegin(), building_map.rend());
+
+        vector<L> ans(n+1, 0);
+
+        ans[0] = 0;
+
+        L minutes = 0;
+        L coordinate = 1;
+
+        for(int i=0; i<n; i++) {
+
+            ans[building_map[i].second + 1] = coordinate;
+
+            minutes += 2 * abs(coordinate) * building_map[i].first;
+
+            if(coordinate < 0) {
+                coordinate = abs(coordinate) + 1;
+            }
+            else {
+                coordinate = -coordinate;
+            }
+        }
+
+        cout<<minutes<<endl;
+
+        for(auto it : ans) {
+            cout<<it<<" ";
+        }
+
+        cout<<endl;
     }
 
     return 0;
